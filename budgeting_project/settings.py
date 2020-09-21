d9e3799766b10ea6c4a4d89dc7388c0bc70500bd
darren <limdarrenj@gmail.com>
2020-09-21 14:18:09 -0700

    small change

--- budgeting_project/settings.py
+++ budgeting_project/settings.py
@@ -142,12 +142,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static'),
 ]
-
-REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
